c8c95717d7f831cf1586172d30e2a80550b258e0
dasebaztian <danielsebastiansanchezm@gmail.com>
2022-10-16 10:16:19 -0500

    Se elimino la función dividirpalabras y se implemento todo en una sola llamada get_peliculas

--- proyectoHIlos.py
+++ proyectoHIlos.py
@@ -13,16 +13,17 @@ class VentanaPrincipal(QMainWindow):
         self.resize(600, 200)
         contenedor = QWidget()
 
-        leftcolumna = QLineEdit()
-        centercolumna = QLineEdit()
-        rightcolumna = QLineEdit()
+        leftcolumna = QImage()
+        centercolumna = QImage()
+        rightcolumna = QImage()
+
 
         lytPrincipal = QGridLayout()
         lblBusca = QLabel("Palabras de peliculas a buscar: ")
 
         self.lnedtTexto = QLineEdit()
         btnBusca = QPushButton("Buscar")
-        btnBusca.clicked.connect(self.dividirTexto)
+        btnBusca.clicked.connect(self.get_peliculas)
 
         lytPrincipal.addWidget(lblBusca, 0, 0)
         lytPrincipal.addWidget(self.lnedtTexto, 0, 1)
@@ -34,35 +35,26 @@ class VentanaPrincipal(QMainWindow):
         contenedor.setLayout(lytPrincipal)
         self.setCentralWidget(contenedor)
 
-
-    def dividirTexto(self):
+    def get_peliculas(self):
+        url_servicio = "http://clandestina-hds.com:8090/movies/title?search="
         palabras = []
         lista = self.lnedtTexto.text()
         for palabras in lista:
             palabras = lista.split(",")
-        self.get_peliculas(palabras)
 
-
-    def get_peliculas(self, lista):
-        url_servicio = "http://clandestina-hds.com:8090/movies/title?search="
-        for i in lista:
+        print(palabras)
+        for i in palabras:
             r = requests.get(url_servicio+i)
             peliculas_data = r.json()
             for pelicula in peliculas_data['results']:
                 print("La pelicula de nombre: {} \n Tiene una URL de imagen: {}".format(pelicula['title'], pelicula["image"]))
-                return pelicula["image"]
+                self.mostrarimagenes(pelicula["image"])
 
-
-
-    def mostrarimagenes(self):
-        url_image = "http://clandestina-hds.com:8090/movies/title?search="
-
+    def mostrarimagenes(self,url_image):
         image = QImage()
         image.loadFromData(requests.get(url_image).content)
         image_label = QLabel()
-        pixmap = QPixmap(image)
-        pixmap2 = pixmap.scaledToWidth(500)
-        image_label.setPixmap(pixmap2)
+        image_label.setPixmap(QPixmap(image))
         image_label.show()
 
 
